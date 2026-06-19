5939455583b59d1d924d01ec6b5f21de95b50f9c
NamrataRoyReinventor <namrata.d.roy@accenture.com>
2026-06-19 10:29:51 +0000

    Bug Fix

--- octofit-tracker/backend/dist/index.js
+++ octofit-tracker/backend/dist/index.js
@@ -1,49 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const activities_1 = __importDefault(require("./routes/activities"));
-const database_1 = require("./config/database");
-const leaderboard_1 = __importDefault(require("./routes/leaderboard"));
-const teams_1 = __importDefault(require("./routes/teams"));
-const users_1 = __importDefault(require("./routes/users"));
-const workouts_1 = __importDefault(require("./routes/workouts"));
-const app = (0, express_1.default)();
-const port = 8000;
-const codespaceName = process.env.CODESPACE_NAME;
-const apiBaseUrl = codespaceName
-    ? `https://${codespaceName}-8000.app.github.dev`
-    : 'http://localhost:8000';
-app.use(express_1.default.json());
-app.get('/health', (_req, res) => {
-    res.json({ status: 'ok', apiBaseUrl });
-});
-app.get('/api', (_req, res) => {
-    res.json({
-        apiBaseUrl,
-        endpoints: [
-            '/api/users/',
-            '/api/teams/',
-            '/api/activities/',
-            '/api/leaderboard/',
-            '/api/workouts/',
-        ],
-    });
-});
-app.use('/api/users', users_1.default);
-app.use('/api/teams', teams_1.default);
-app.use('/api/activities', activities_1.default);
-app.use('/api/leaderboard', leaderboard_1.default);
-app.use('/api/workouts', workouts_1.default);
-const startServer = async () => {
-    await (0, database_1.connectDatabase)();
-    app.listen(port, '0.0.0.0', () => {
-        console.log(`OctoFit backend listening on ${apiBaseUrl}`);
-    });
-};
-startServer().catch((error) => {
+const server_1 = require("./server");
+(0, server_1.startServer)().catch((error) => {
     console.error('Failed to start backend:', error);
     process.exit(1);
 });
